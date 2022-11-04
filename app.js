ffa7cc6f78e1422e3b3bc6640c5bc4c1edcc7001
stallingerl <lena.stallinger@webanizer.de>
2022-11-04 08:26:24 +0000

    typo in node commands

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var data = {
-  title: 'Study sync',
+  title: 'Study sync V1',
   selected: null,
   question: 'In the Battle of Wolf 359 what prominent Miranda-class ship was destroyed?',
   choices: [
